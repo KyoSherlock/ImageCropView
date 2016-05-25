5899df322a29ef1b01674a0f62b27a1a2d37219d
Sherlock.jiang <jianghui-2000@163.com>
2016-05-25 21:59:12 +0800

    add License

--- ImageCropView-library/src/main/java/com/kyo/imagecrop/CropLayout.java
+++ ImageCropView-library/src/main/java/com/kyo/imagecrop/CropLayout.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2015, KyoSherlock
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.kyo.imagecrop;
 
 import java.io.FileNotFoundException;
@@ -403,11 +418,11 @@ public class CropLayout extends FrameLayout {
 		return (int) Math.max(outMetrics.heightPixels, outMetrics.widthPixels);
 	}
 
-	public static interface OnCropListener {
-		public void onCropResult(Uri data);
+	public interface OnCropListener {
+		void onCropResult(Uri data);
 
-		public void onCropFailed(String errmsg);
+		void onCropFailed(String errmsg);
 
-		public void onLoadingStateChanged(boolean isLoading);
+		void onLoadingStateChanged(boolean isLoading);
 	}
 }
